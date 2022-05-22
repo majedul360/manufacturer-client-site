3886e3046700290f4171640e9e290a1e9093d92e
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-05-22 08:59:40 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Header from "./shared/header/Header";
+import Purchase from "./pages/purchase/Purchase";
 function App() {
-  return <div className="">sfss</div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/purchase" element={<Purchase />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,19 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          black: "#171b1f",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+  plugins: [],
   plugins: [require("daisyui")],
 };
