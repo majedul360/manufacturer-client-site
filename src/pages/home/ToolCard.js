50daa0d1c715506a5f06c62bb602da96212efd11
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-05-23 13:34:01 +0600

    protected route setup done

--- src/pages/home/ToolCard.js
+++ src/pages/home/ToolCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const ToolCard = ({ product }) => {
-  const { name, img, price, minOrder, available, desc } = product;
+  const { _id, name, img, price, minOrder, available, desc } = product;
   const navigate = useNavigate();
   return (
     <div class="card  bg-base-100 shadow-xl">
