acc96d633ceb071b26f4d15c4963b7bc4cca223d
Majedul Islam Majedul Islalm <majedulislam393@gmail.com>
2022-10-03 17:51:45 +0600

    again start

--- src/firebase/Firebase.int.js
+++ src/firebase/Firebase.int.js
@@ -6,12 +6,18 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+  apiKey: "AIzaSyBnc-04M_NjKEi75LdHeWGq63mQRPMMH7M",
+  authDomain: "woodstore-23db8.firebaseapp.com",
+  projectId: "woodstore-23db8",
+  storageBucket: "woodstore-23db8.appspot.com",
+  messagingSenderId: "1004021355389",
+  appId: "1:1004021355389:web:df373a2298618d9459f214",
+  // apiKey: process.env.REACT_APP_apiKey,
+  // authDomain: process.env.REACT_APP_authDomain,
+  // projectId: process.env.REACT_APP_projectId,
+  // storageBucket: process.env.REACT_APP_storageBucket,
+  // messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  // appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
